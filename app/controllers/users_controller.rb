915b64cb7cf3fb95977e82273fb0833d2e0d806a
Kyle <kyleludlow@gmail.com>
2016-12-15 11:32:08 -0800

    adds immediate sign in up new user sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,8 +14,10 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      # logs user in immediately
+      session[:user_id] = @user.id
       flash[:success] = "Welcome to the alpha blog #{@user.username}"
-      redirect_to articles_path
+      redirect_to user_path(@user)
     else
       render 'new'
     end
